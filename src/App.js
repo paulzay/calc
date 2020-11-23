f52e997a5be833c45dfd655c07bf0df9d1e95dfd
Restyled.io <commits@restyled.io>
2020-11-23 15:11:56 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (<div className = "App"><header>Calculator App</header>
-    </div>);
+  return (
+    <div className="App">
+      <header>Calculator App</header>
+    </div>
+  );
 }
 
 export default App;
