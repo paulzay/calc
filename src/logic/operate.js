55510f8569ecdc703417f82a1d4c8db92f7f8cd7
paulzay <paulotieno2@gmail.com>
2020-11-25 16:56:03 +0300

    implement operate method

---
+++ src/logic/operate.js
@@ -0,0 +1,25 @@
+import Big from 'big.js';
+
+export default function operate(numberOne, numberTwo, operation) {
+  let result;
+  const a = Big(numberOne);
+  const b = Big(numberTwo);
+
+  if (operation === '+') {
+    result = a.plus(b);
+  } else if (operation === '-') {
+    result = a.minus(b);
+  } else if (operation === 'x') {
+    result = a.times(b);
+  } else if (operation === '÷') {
+    if (numberTwo !== '0') {
+      result = a.div(b);
+    } else {
+      return 'Error';
+    }
+  } else if (operation === '%') {
+    result = a.times(0.01);
+  }
+
+  return result;
+}
