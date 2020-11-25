35b5da1a0cad466782c542dde46abd5e56136dcc
paulzay <paulotieno2@gmail.com>
2020-11-25 12:02:57 +0300

    import components to the app.js file

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import ButtonDisplay from './ButtonDisplay';
+import Display from './Display';
+
+function App() {
+  return (
+    <>
+      <Display />
+      <ButtonDisplay />
+    </>
+  );
+}
+
+export default App;
