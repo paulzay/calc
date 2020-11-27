cd07598efa385fd8ed453733d8257ddc12bd63a9
paulzay <paulotieno2@gmail.com>
2020-11-27 22:37:04 +0300

    convert results to string before returning

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -6,20 +6,19 @@ export default function operate(numberOne, numberTwo, operation) {
   const b = Big(numberTwo);
 
   if (operation === '+') {
-    result = a.plus(b);
+    result = (a.plus(b)).toString();
   } else if (operation === '-') {
-    result = a.minus(b);
-  } else if (operation === 'x') {
-    result = a.times(b);
-  } else if (operation === '÷') {
+    result = (a.minus(b)).toString();
+  } else if (operation === 'X') {
+    result = (a.times(b)).toString();
+  } else if (operation === '/') {
     if (numberTwo !== '0') {
-      result = a.div(b);
+      result = (a.div(b)).toString();
     } else {
       return 'Error';
     }
   } else if (operation === '%') {
-    result = a.div(100);
+    result = (a.div(100)).toString();
   }
-
   return result;
 }
